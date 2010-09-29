1435869b192b381fac5a2d2001235083595520bb
Łukasz Stelmach <lukasz.stelmach@iem.pw.edu.pl>
2010-09-29 19:38:12 +0200

    DND support: OpenStreetMap, Google Maps, geo URI.
    Receive URIs from OpenStreetMap, Google Maps or geo URIs, and set
    GEO property accordingly.

--- contrib/lisp/org-geo.el
+++ contrib/lisp/org-geo.el
@@ -4,7 +4,7 @@
 
 ;; Author: Łukasz Stelmach <lukasz.stelmach@iem.pw.edu.pl>
 ;; Created: 2010-09-29
-;; Version: 0.0
+;; Version: 0.1
 
 ;; This file is not (yet) part of GNU Emacs.
 
@@ -25,5 +25,97 @@
 ;; org-geo.el provides utility functions to support geographic
 ;; information stored in the GEO property.
 
+;; Loading the module makes org-mode buffers accept links with
+;; geographic coordinates to OpenStreetMap and Google Maps, as well as
+;; the geo URIs (see: http://geouri.org). GEO property is set on the
+;; node that a URI is dropped on. Note that if one drops above the
+;; first node in a buffer then the the property is set on that node.
+
+;; Note: a link dropped must be of a link type in terms of DND system
+;; not just a piece of text, however, support for plain text
+;; containing coordinates is also possible.
+
 ;;; Usage:
 ;;; (require 'org-geo)
+
+;; Regular expression to verify geo URI. See RFC 5870, section
+;; 3.3. It may suboptimal considering regular expression engine but
+;; it is 100% RFC compliant. However, it doesn't as yet provide
+;; coordinate range verification.
+(defconst org-geo-uri-regexp
+  (let* ((alphanum "[A-Za-z0-9]")
+	 (p-unreserved "[\[\]:&+$]")
+	 (pct-encoded "%[[:xdigit:]][[:xdigit:]]")
+
+	 (mark "[-_.!~*'()]")
+	 (unreserved (concat "\\(?:" alphanum "\\|" mark "\\)"))
+	 (pnum "[0-9]+\\(?:\.[0-9]+\\)?")
+	 (num (concat "-?" pnum))
+	 (labeltext (concat "\\(?:" alphanum "\\|-\\)+"))
+
+	 (pvalue (concat "\\(?:"
+			 p-unreserved "\\|"
+			 unreserved "\\|"
+			 pct-encoded "\\)+" ))
+	 (pname labeltext)
+	 (parameter (concat ";" pname "\\(:?=" pvalue "\\)?"))
+	 (uval pnum)
+	 (uncp (concat ";u=" uval))
+	 (crslabel (concat "\\(?:wgs84\\|" labeltext "\\)"))
+	 (crsp (concat ";crs=" crslabel))
+	 (p (concat
+	     "\\(" crsp "\\)?"
+	     "\\(" uncp "\\)?"
+	     "\\(" parameter "\\)*" ))
+
+	 (coord-c num)
+	 (coord-b num)
+	 (coord-a num)
+	 (coordinates (concat "\\(" coord-a "\\),"
+			      "\\(" coord-b "\\)"
+			      "\\(," coord-c "\\)?"))
+	 (geo-path (concat coordinates p))
+	 (geo-scheme "geo"))
+    (concat geo-scheme ":" geo-path)))
+
+(defun org-geo-dnd-set-geo-property (uri action)
+  "Receive URI from Emacs' DND system and set GEO property of the
+current node to a geo URI with coordiantes extracted from the
+dropped URI. If a geo URI is dropped then use it directly. We
+support links from OpenStreetMap and Google Maps."
+  (save-excursion
+    (unless (outline-previous-heading)
+      (search-forward-regexp (concat "^" org-outline-regexp)))
+    (org-set-property "GEO"
+      (cond
+					; OpenStreetMap
+       ((string-match
+	 "^http://\\(?:www\.\\)openstreetmap\.org/" uri)
+	(let (lat lon)
+	  (dolist (p (split-string (cadr (split-string uri "\?")) "&" ))
+	    (cond
+	     ((string-match "lat=\\(-?[.0-9]+\\)" p)
+	      (setq lat (match-string 1 p)))
+	     ((string-match "lon=\\(-?[.0-9]+\\)" p)
+	      (setq lon (match-string 1 p)))))
+	  (concat "geo:" lat "," lon)))
+					; Google Maps
+       ((string-match
+	 "^http://maps\.google\.com/.*ll=\\(-?[.0-9]+\\),\\(-?[.0-9]+\\)" uri)
+	(concat "geo:" (match-string 1 uri) "," (match-string 2 uri)))
+					; geo URI
+       ((string-match (concat "^" org-geo-uri-regexp) uri) uri)))))
+
+(defun org-geo-dnd-support ()
+  (org-set-local
+   'dnd-protocol-alist
+   (append
+    (list (cons (concat "^\\(http://"
+			"\\(?:\\(?:www\.\\)?openstreetmap\.org"
+			"\\|"
+			"maps\.google\.com\\)"
+			"\\|" org-geo-uri-regexp "\\)")
+		'org-geo-dnd-set-geo-property))
+    'dnd-protocol-alist)))
+
+(add-hook 'org-mode-hook 'org-geo-dnd-support)
