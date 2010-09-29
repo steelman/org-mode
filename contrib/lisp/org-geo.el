09732c1a1c1c017d480fe15992e8011dc2ac1429
Łukasz Stelmach <lukasz.stelmach@iem.pw.edu.pl>
2010-09-29 12:12:57 +0200

    Empty org-geo.el file.

---
+++ contrib/lisp/org-geo.el
@@ -0,0 +1,29 @@
+;;; org-geo.el --- geolocation utilities for Org.
+
+;; Copyright (C) 2010 Łukasz Stelmach
+
+;; Author: Łukasz Stelmach <lukasz.stelmach@iem.pw.edu.pl>
+;; Created: 2010-09-29
+;; Version: 0.0
+
+;; This file is not (yet) part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation version 2.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; For a copy of the GNU General Public License, search the Internet,
+;; or write to the Free Software Foundation, Inc., 59 Temple Place,
+;; Suite 330, Boston, MA 02111-1307 USA
+
+;;; Commentary:
+;; org-geo.el provides utility functions to support geographic
+;; information stored in the GEO property.
+
+;;; Usage:
+;;; (require 'org-geo)
